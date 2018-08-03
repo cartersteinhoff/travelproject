cb5001e99478b2d450df35e0dd08d1248c94402a
kaur123 <gunnypari@gmail.com>
2018-08-03 15:14:08 -0700

    i changes

--- apps/travel_app/models.py
+++ apps/travel_app/models.py
@@ -1,3 +1,61 @@
 from django.db import models
-from django.db import models
-# Create your models here.
+import bcrypt,datetime
+
+class UserManager(models.Manager):
+    def validate_registration(self, postData):
+        response = {
+           'status' : False,
+           'errors' : []
+        }
+        if len(postData['first_name']) < 2:
+           response['errors'].append("first_name too short")
+
+        if len(postData['last_name']) < 2:
+           response['errors'].append("last_name too short")
+
+        if len(postData['email']) < 10:
+           response['errors'].append("invalid email")
+
+        if len(postData['password']) < 8:
+            response['errors'].append("invalid password")
+
+        if postData['confirm_pw'] != postData['password']:
+            response['errors'].append("invalid password")
+            
+
+        if len(response['errors']) == 0:
+            response['status'] = True
+            response['user_id'] = User.objects.create(
+                first_name=postData['first_name'],
+                last_name=postData['last_name'],
+                email=postData['email'],
+                password=bcrypt.hashpw(postData['password'].encode(), bcrypt.gensalt())
+            ).id
+        return response 
+
+    def validate_login(self, postData):
+        response = {
+           'status' : False,
+           'errors' : []
+        }
+        #if len(User.objects.filter(eamil=postData['email'])) == 0:
+        existing_users = User.objects.filter(email=postData['email'])
+        if len(existing_users) == 0:
+            print('errors')
+            response['errors'].append("invalid input")
+        else:
+            if bcrypt.checkpw(postData['password'].encode(), existing_users[0].password.encode()):
+                response['status'] = True
+                response['user_id'] = existing_users[0].id
+            else:
+                print("invalid input")
+        return response    
+          
+class User(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+    objects = UserManager()
--- apps/travel_app/views.py
+++ apps/travel_app/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render, HttpResponse, redirect
+from .models import *
+from django.contrib import messages
+
+def index(request):
+    return render(request,'travel_app/login-reg.html')
+
+def processreg(request):
+    result = User.objects.validate_registration(request.POST)
+    if result['status']:    #that means if its true
+        request.session['user_id'] = result['user_id']
+        return redirect('/success')
+    else:
+        for error in result['errors']:
+            messages.error(request,error)   
+    return redirect('/')
+
+
+def processlog(request):
+    result = User.objects.validate_login(request.POST)
+    if result['status']: #that means if its true
+        request.session['user_id'] = result['user_id']
+        return redirect('/success')
+    else:
+        for error in result['errors']:
+            messages.error(request,error)    
+    return redirect('/')
+
+def success(request):
+    if 'user_id' not in request.session:
+        return redirect('/')
+    
+    return render(request, 'travel_app/success.html')
+
+def logout(request):
+    request.session.clear() 
+    return redirect('/')
