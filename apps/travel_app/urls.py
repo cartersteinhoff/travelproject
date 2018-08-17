560fb777d019e1cf331d5e02155917fc5c681139
kaur123 <gunnypari@gmail.com>
2018-08-16 20:55:28 -0700

    test

--- apps/travel_app/urls.py
+++ apps/travel_app/urls.py
@@ -2,8 +2,14 @@ from django.conf.urls import url
 from . import views          
 urlpatterns = [
     url(r'^$', views.index),
+    url(r'^registration$', views.registration),
     url(r'^processreg$',views.processreg),
     url(r'^processlog$', views.processlog),
     url(r'^success$',views.success),
     url(r'^logout$', views.logout),
+    url(r'^home$', views.home),
+    url(r'^add$',views.add),
+    url(r'^add_travel_plan$', views.add_travel_plan),
+    url(r'^details/(?P<trip_id>\d+)$', views.details),
+    url(r'^join/(?P<trip_id>\d+)$', views.join)
 ]
--- apps/travel_app/views.py
+++ apps/travel_app/views.py
@@ -4,7 +4,10 @@ from .models import *
 from django.contrib import messages
 
 def index(request):
-    return render(request,'travel_app/login-reg.html')
+    return render(request,'travel_app/login.html')
+
+def registration(request):
+    return render(request, 'travel_app/login-reg.html')
 
 def processreg(request):
     result = User.objects.validate_registration(request.POST)
@@ -14,7 +17,7 @@ def processreg(request):
     else:
         for error in result['errors']:
             messages.error(request,error)   
-    return redirect('/')
+    return redirect('/registration')
 
 
 def processlog(request):
@@ -34,17 +37,13 @@ def success(request):
     context = {
         "me": User.objects.get(id=request.session['user_id']),
         "users": User.objects.all(),
-        # "all_trips": Trip.objects.all(),
-        # # "not_my_trips": Trip.objects.exclude(user_on_trip=request.session['user_id']),
-        # # "my_trips": Trip.objects.filter(user_on_trip=request.session['user_id'])
-        # "my_trips": User.objects.get(id=request.session['user_id']).created_trips.all(),
-        # "joined_trips": User.objects.get(id=request.session['user_id']).trips.all(),
-        # "others_trip":Trip.objects.exclude(created_by= User.objects.filter(id=request.session['user_id'])).exclude(user_on_trip=User.objects.get(id=request.session['user_id'])) 
+        "all_trips": Trip.objects.all(),
+        # "not_my_trips": Trip.objects.exclude(user_on_trip=request.session['user_id']),
+        # "my_trips": Trip.objects.filter(user_on_trip=request.session['user_id']),
+        "my_trips": User.objects.get(id=request.session['user_id']).created_trips.all(),
+        "joined_trips": User.objects.get(id=request.session['user_id']).trips.all(),
+        "others_trip":Trip.objects.exclude(created_by= User.objects.filter(id=request.session['user_id'])).exclude(user_on_trip=User.objects.get(id=request.session['user_id'])) 
 
-    }
-    
-    context = {
-            "me": User.objects.get(id=request.session['user_id'])
     }
     
     return render(request, 'travel_app/success.html', context)
@@ -52,3 +51,40 @@ def success(request):
 def logout(request):
     request.session.clear() 
     return redirect('/')
+
+def home(request):
+    return redirect('/success')
+    
+def add_travel_plan(request):
+    if 'user_id' not in request.session:
+        return redirect('/')
+
+    return render(request, 'travel_app/add_trip.html')
+
+def add(request):
+    result = Trip.objects.add(request.POST, request.session['user_id'])
+    if result['status']:    
+        return redirect('/success')
+    else:
+        for error in result['errors']:
+            messages.error(request,error) 
+    return redirect('/add_travel_plan') 
+
+def details(request, trip_id):
+    if 'user_id' not in request.session:
+        return redirect('/')
+
+    trip = Trip.objects.get(id=trip_id)
+    
+    context = {
+        "other_users" : User.objects.filter(trips=trip_id).exclude(created_trips=trip_id),
+        "current_trip": trip #or without variable "current_trip": Trip.objects.get(id=trip_id)
+    }
+    print(trip_id)
+    return render(request,'travel_app/details.html', context)
+
+def join(request, trip_id):
+    Trip.objects.join(trip_id,request.session['user_id'])
+
+    return redirect('/success')
+
